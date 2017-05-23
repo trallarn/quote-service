6b59f962469590d7ba469899e2f5da6289a9fdbf
trallarn <trallarn@yahoo.com>
2017-05-23 10:01:58 +0200

    adds test for weekly and monthly

--- test/src/QuoteRepositoryTest.js
+++ test/src/QuoteRepositoryTest.js
@@ -8,11 +8,34 @@ test.onFinish(function(){
     mongoFactory.closeEquityDb();
 });
 
-test('getAsync', function(t) {
+test('getAsync default daily', function(t) {
     t.plan(1);
 
     quoteRepository.getAsync('ERIC-B.ST', false, false)
-        .then(function(quotes) {
+        .then(quotes => {
             t.ok(quotes.length > 0, 'quotes no empty');
+        });
+});
+
+test('getAsync weekly', function(t) {
+    t.plan(3);
+
+    quoteRepository.getAsync('ERIC-B.ST', false, false, { period: 'weekly' })
+        .then(quotes => {
+            t.ok(quotes.length > 0, 'quotes no empty');
+            const first = quotes[0];
+            t.ok(first._id.year, 'expected year in _id');
+            t.ok(first._id.week, 'expected week in _id');
+        });
+});
+test('getAsync monthly', function(t) {
+    t.plan(3);
+
+    quoteRepository.getAsync('ERIC-B.ST', false, false, { period: 'monthly' })
+        .then(quotes => {
+            t.ok(quotes.length > 0, 'quotes no empty');
+            const first = quotes[0];
+            t.ok(first._id.year, 'expected year in _id');
+            t.ok(first._id.month, 'expected month in _id');
         });
 });
